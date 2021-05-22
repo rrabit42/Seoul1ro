c7cc87a58f7adcc922de3fc63df64c028856e350
angelkim88 <66551410+hoharon@users.noreply.github.com>
2021-05-23 08:51:05 +0900

    #1.0 프론트엔드 구현 완료

--- main/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from main.models import Search
-
-
-class InputForm(forms.ModelForm):
-    class Meta:
-        model = Search
-        fields = '__all__'
--- main/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from main.models import Search
-
-
-class SearchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Search
-        fields = '__all__'
