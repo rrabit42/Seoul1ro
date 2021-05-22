8e90ccd2de487030d55ae14f5aefba9f3f87c52e
rrabit42 <gegiraffe@gmail.com>
2021-05-23 02:17:44 +0900

    [feat] static file 관련 설정

--- Seoul1ro/urls.py
+++ Seoul1ro/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('', ShowResult),
     # path('', include(router.urls)),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
